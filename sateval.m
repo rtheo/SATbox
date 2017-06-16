20042e9c2aea46679993b120ac9a51223ae5067d
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-06-16 12:46:03 +0300

    Update sateval.m

--- sateval.m
+++ sateval.m
@@ -10,9 +10,8 @@ if isempty( over )
     for i=1:nc,  chi(i, :) = floor( mod( floor( literals/cp(i) ), cc(i) )/( cc(i) - 1) ); end
     c = all( 1 - chi, 1 ); d = any( chi, 1 );
 else
-    chi = zeros(Lo, dim); c = zeros(1, dim);d = c;
-    for i=1:Lo, chi(i, :) = eta( int, over(i), over(i), 1 ); end
-    imagesc(chi), colormap gray
+    chi = zeros(Lo, dim); c = zeros(1, dim);d = c; notint = dim - int - 1;
+    for i=1:Lo, chi(i, :) = eta( int, over(i), over(i), 1 ); chi(i, :) = eta( notint, over(i), over(i), 1 ); end
     Filter = find( all( chi, 1 ) == 1 ); 
     literals = bitxor(nots, Filter ); 
     chi = zeros(nc, length(literals));
