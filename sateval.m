5080a720dde04e4766e1bc92706df7217bdf1d58
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-06-22 20:31:30 +0300

    Update sateval.m

--- sateval.m
+++ sateval.m
@@ -4,21 +4,21 @@ function [c, d] = sateval( clauses, nots, over, int, nint, nc, no)
 % Caution: Displaying the outputs may still cause problems for large parameter values.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 c = zeros(1, length(int) );d = c; optr = int+1; 
-if no > 0, optr = oxfilter( int, nint, over, length(int), no ); end
+if no > 0, optr = trimmer( int, nint, over, length(int), no ); end
 literals = bitxor( nots, optr ); % apply negation code to atoms
 cp = 2.^cumsum( clauses - 1 ); cc = 2.^clauses;
 c( optr ) = UI( literals, cp, cc, nc, 1); 
 d( optr ) = 1 - UI( literals, cp, cc, nc, 0); 
 end
 
-function psi = oxfilter( int, nint, oc, dim, no )
+function psi = trimmer( int, nint, oc, dim, no )
 % Search space trimming filter for overlaps
 psi = zeros(1, dim); 
 for i=1:no 
     psi = psi + ( bitor( int, dim - oc(i) - 1 ) == dim-1 );
     psi = psi + ( bitor( nint, dim - oc(i) - 1 ) == dim-1 ); 
 end
-psi = find( psi == 1 );
+psi = find( psi == 0 );
 end
 
 function chi = UI(literals, cp, cc, nc, flag)
