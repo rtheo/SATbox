10d7a74a5487112fcc13404cd3ac397f795cbacd
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-06-29 21:22:13 +0300

    Add files via upload

---
+++ satassign.m
@@ -0,0 +1,32 @@
+function [ocodes, Lo] = satassign( atoms, nots, Lc, L )
+% Find connectivity matrix for assignment map
+La = length( unique( atoms ) ); % number of unique atoms  
+c = zeros(La, L); idx = [atoms; 1:L];
+if isempty( nots ), nots = ones(1, L); end
+for i=1:L, c( idx(1, i), idx(2, i) ) = nots(i);end
+degeneracies = find( sum( abs(c), 2 ) > 1 ); 
+deglen = length(degeneracies);
+maxdeg = max( sum(c, 2) );
+base = 2.^(0:L-1);
+if  isempty(degeneracies), ocodes = [];Lo = 0; end
+ocodes = zeros( 1, deglen );
+for i=1:deglen    
+    pick = abs( c( degeneracies(i), :) ); 
+    Deg = sum( abs( pick ) );
+    ocodes(i) = sum( pick.*base );
+end
+Lo = length(ocodes);
+satclass = ( Lc > 1) + 2*( ~isempty(degeneracies));
+figure(1), imagesc(c)
+ylabel 'Atoms', xlabel 'Literals'
+title(['ASSIGNMENT: SAT Class = ', num2str( satclass ),' Atoms: ',num2str(La),' Overlaps: ',num2str( Lo ), ' (PRESS ENTER)'])
+pause
+clc, clf, close all
+if Lo == L
+    ylabel 'Atoms', xlabel Literals: 'Unsatisfiable problem! (Overlaps filter out all significance levels)')
+    yn = '0';
+    while ~strcmp(yn, 'y')||~strcmp(yn,'n') 
+        yn = input('Interrupt? (y/n)');
+    end
+end
+end
