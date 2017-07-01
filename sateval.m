4e091c8299ff8a17e6c28b8cc7eda3d5634e0727
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-07-01 14:56:41 +0300

    Update sateval.m

--- sateval.m
+++ sateval.m
@@ -1,11 +1,12 @@
 function [cnf, dnf] = sateval( clauses, nots, ocodes, dim, option )
 save = bin2dec( num2str( floor( (nots + 1)/2 ) ) ); %floor( (nots + 1)/2 )*(2.^(0:L-1))';
 int = 0:dim-1;overset = int;
+nc = length( clauses );
 runlength = (dim - 2)*option + 1; 
 cc = 2.^clauses; cs = 2.^cumsum( [0, clauses] );
 dnf = zeros( runlength+option, dim );cnf = dnf;  
 if ~isempty(ocodes)
-    % Valid for SAT2-3: functional composition of negations - overlaps necessary
+    % Valid for SAT2-3: functional composition of negations on overlaps necessary
     overset = trimmer( int, dim - int - 1, ocodes, dim - ocodes - 1 );
     for nots = 1-option:runlength
         if runlength==1, arg = save; else arg = nots; end
@@ -14,13 +15,13 @@ if ~isempty(ocodes)
         dnf( nots+option, :) = UIeval( literals, cs, cc, 1);
     end
 else
-    % Valid for SAT0-1: No reason to apply the indicator more than once - negations = permutations
+    % Valid for SAT0-1: No reason to apply the indicator more than once - negations:=permutations
     cnfset = UIeval( int, clauses, 0);
     dnfset = UIeval( int, clauses, 1);
     for nots = 1-option:runlength
         if runlength==1, arg = save; else arg = nots; end
         literals = bitxor( int, arg ); 
-        cnf( nots+option, :) = cnfset( literals+1 ); 
+        cnf( nots+option, :) = cnfset(literals+1); 
         dnf( nots+option, :) = dnfset(literals+1); 
     end
 end
