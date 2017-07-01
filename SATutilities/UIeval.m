6b2ead4c7451299252b265611cd8ea8dba415b70
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-07-01 12:41:40 +0300

    Add files via upload

---
+++ SATutilities/UIeval.m
@@ -0,0 +1,10 @@
+function chi = UIeval( literals, cs, cc, flag)
+% SAT universal indicator function, flag (0/1): CNF/DNF
+chi = zeros( 1, length(literals) );nc = length(clauses); 
+for i=1:nc, chi = chi + indicatrix(  mod( floor( literals/cs(i) ), cc(i) ), cc(i) - 1, flag ); end
+if flag, chi = chi > 0; else chi = floor( chi/nc ); end
+end
+
+function y = indicatrix( x, cc, flag )
+if flag,  y = 1 - floor( 1 - x./cc ); else y = floor( x./cc ); end
+end
