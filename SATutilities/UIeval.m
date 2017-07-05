b5ffd45a7d626e8b410befc7d756d8a01bd15e7d
Theophanes Raptis <rtheo@dat.demokritos.gr>
2017-07-05 21:27:00 +0300

    Update UIeval.m

--- SATutilities/UIeval.m
+++ SATutilities/UIeval.m
@@ -2,9 +2,9 @@ function chi = UIeval( literals, cs, cc, nc, flag)
 % SAT universal indicator function, flag (0/1): CNF/DNF
 chi = zeros( 1, length(literals) );
 for i=1:nc, chi = chi + indicatrix(  mod( floor( literals/cs(i) ), cc(i) ), cc(i) - 1, flag ); end
-if flag, chi = chi > 0; else chi = floor( chi/nc ); end
+if flag, chi = chi > 0;  else chi = floor( chi/nc );  end
 end
 
 function y = indicatrix( x, div, flag )
-if flag,  y = 1 - floor( 1 - x./div ); else y = floor( x./div ); end
+if flag, y = floor( x./div ); else  y = x > 0; end
 end
